6f699ef64046aa37f004bf44fcc02b6d80698156
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-04 10:37:22 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4177,7 +4177,6 @@ class PlayState extends MusicBeatState
 
 					if (SONG.validScore)
 					{
-						NGio.unlockMedal(60961);
 						Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 					}
 
