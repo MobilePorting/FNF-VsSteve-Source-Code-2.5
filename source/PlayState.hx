ae92f944f8389bc46b4026e61384ef5e420cfa5f
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-05 20:17:14 +0300

    camera goes brrrr

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1515,7 +1515,7 @@ class PlayState extends MusicBeatState
 		//	camHUD.setFilters([new ShaderFilter(vgs)]);
 		//}
 
-		FlxG.camera.follow(camFollow, LOCKON, 0.04 * (30 / (cast (Lib.current.getChildAt(0), Main)).getFPS()) #if mobile + 1 #end);
+		FlxG.camera.follow(camFollow, LOCKON, 0.04 * (30 / (cast (Lib.current.getChildAt(0), Main)).getFPS()#if mobile + 1 #end));
 		// FlxG.camera.setScrollBounds(0, FlxG.width, 0, FlxG.height);
 		FlxG.camera.zoom = defaultCamZoom;
 		FlxG.camera.focusOn(camFollow.getPosition());
