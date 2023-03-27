83500542207238ab823b3e1804186ffe617adb06
mcagabe19 <egzozu.be.bas@gmail.com>
2023-03-27 19:30:01 +0300

    middle fucking scroll

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2966,6 +2966,13 @@ class PlayState extends MusicBeatState
 			babyArrow.animation.play('static');
 			babyArrow.x += 50;
 			babyArrow.x += ((FlxG.width / 2) * player);
+			
+			if (FlxG.save.data.middleScroll) {
+				babyArrow.x -= 275;
+				if (player != 1) {
+					babyArrow.visible = false;
+				}
+			}
 
 			cpuStrums.forEach(function(spr:FlxSprite)
 			{					
